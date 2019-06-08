f2880f87c0ed248503b4e728d859c0457c7f4da9
pablomm <pablo.mark@hotmail.com>
2019-06-08 11:30:18 +0200

    starting latex writting

---
+++ figuras/0-empty.py
@@ -0,0 +1,7 @@
+
+
+import matplotlib.pyplot as plt
+
+plt.figure("0-empty")
+plt.text(.5, .5, "Figure", horizontalalignment='center')
+plt.show()
---
+++ figures/0-empty.py
@@ -0,0 +1,9 @@
+
+
+import matplotlib.pyplot as plt
+
+
+plt.figure("0-empty")
+
+plt.text(.5, .5, "Figure")
+plt.show()
