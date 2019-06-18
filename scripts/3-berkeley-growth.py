129d84659c20b435f5b911a81a5bcce45a811a79
pablomm <pablo.marcosm@estudiante.uam.es>
2019-06-18 20:31:34 +0200

    Capital letters and typos

--- scripts/3-berkeley-growth.py
+++ scripts/3-berkeley-growth.py
@@ -17,12 +17,14 @@ fd = fd[data['target'] == 0]
 fd = fd.derivative()
 fd.interpolator = SplineInterpolator(3)
 fd.dataset_label = None
-fd.axes_labels = ["age", r"$\partial \, height \, / \, \partial \, age$"]
+fd.axes_labels = ["age (year)", r"$\partial \, height \, / \, \partial \, age$ (cm/year)"]
 
 plt.figure("berkeley-males")
 fd.plot()
 
 plt.xlim(fd.domain_range[0])
+a,b = plt.ylim()
+plt.ylim(a, 20)
 
 plt.tight_layout()
 
@@ -33,6 +35,8 @@ w.plot()
 plt.tight_layout()
 plt.figure("berkeley-registered")
 fd.compose(w).plot()
+plt.ylim(a, 20)
+plt.xlim(fd.domain_range[0])
 
 plt.tight_layout()
 plt.show()
